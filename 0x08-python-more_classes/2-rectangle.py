b862ff226cfe0c3278bc5b688fc8d600cefeac2e
myrajarenga <myrajarenga1234@gmail.com>
2023-01-03 06:05:53 -0800

    a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,8 +44,9 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     def area(self):
-       return self.__height * self.__width
+        return self.__height * self.__width
 
     def perimeter(self):
         if self.__height == 0 or self.__height == 0:
