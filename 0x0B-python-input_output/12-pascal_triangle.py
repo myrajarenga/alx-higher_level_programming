8bec9372d0570083c1b39283bb36e8946b27226c
myrajarenga <myrajarenga1234@gmail.com>
2023-01-12 12:03:29 -0800

    Pascals traingle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,13 +12,13 @@ def pascal_triangle(n):
         my_list = []
         return my_list
     else:
-        list_i =[[1]]
-        for i in range (n -1):
+        list_i = [[1]]
+        for i in range(n - 1):
             temp = [0] + list_i[-1] + [0]
 
             row = []
 
-            for j in range(len(list_i[-1])+ 1):
-                row.append(temp[j] + temp[j +1])
+            for j in range(len(list_i[-1]) + 1):
+                row.append(temp[j] + temp[j + 1])
             list_i.append(row)
         return list_i
