e67f78370ce2446da1c4de34da3f7aefaca6a086
myrajarenga <myrajarenga1234@gmail.com>
2023-02-15 12:13:09 -0800

    inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- script that list all cities of astate
-SELECT cities.id, cities.name
-FROM cities, states
-INNER JOIN states on cities.state_id=state.id
+SELECT cities.id, cities.name states.name
+FROM cities
+INNER JOIN states ON cities.state_id=state.id
 ORDER BY cities.id ASC;
