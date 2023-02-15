f28881c2d351cca88eaec120eab944ee76e3fadf
myrajarenga <myrajarenga1234@gmail.com>
2023-02-15 12:06:44 -0800

    create database

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
--- creating database 
+-- create a database and a user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- create user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_od_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
