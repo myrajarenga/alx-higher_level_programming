fda4c6b880aa82fd5af042dfa7ce3f0b04c7166b
myrajarenga <myrajarenga1234@gmail.com>
2023-02-15 06:57:34 -0800

    script that lists cities ofastate fround in adatabse

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- script that list all cities of astate
 SELECT cities.id, cities.name
 FROM cities, states
-WHERE cities.state_id = state.id AND states.name = 'California'
+WHERE cities.state_id = states.id AND states.name = 'California'
 ORDER BY cities.id ASC;
