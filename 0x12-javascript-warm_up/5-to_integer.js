414042ca9a13eeaa622af67ecf8884e3e0cb2551
myrajarenga <myrajarenga1234@gmail.com>
2023-03-14 02:08:11 -0700

    an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 if (isNaN(Math.trunc(process.argv[2]))) {
-  console.log('Not a number: ');
+  console.log('Not a number');
 } else {
-  console.log('My number:' + Math.trunc(process.argv[2]));
+  console.log('My number: ' + Math.trunc(process.argv[2]));
 }
+
