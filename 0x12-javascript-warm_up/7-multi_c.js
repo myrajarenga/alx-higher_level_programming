e624510c99e90def61b3bb68df8fe06828ec4615
myrajarenga <myrajarenga1234@gmail.com>
2023-03-14 02:42:02 -0700

    looping

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,6 +4,6 @@ if (isNaN(Math.trunc(process.argv[2]))) {
   console.log('Missing number of occurnces');
 } else {
   for (let i = 0; i < process.argv[2]; i++) {
-   console.log('C s fun');
- }
+    console.log('C s fun');
+  }
 }
