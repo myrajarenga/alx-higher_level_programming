6b3527e72d168f582772cbb3f80a91da009812b0
myrajarenga <myrajarenga1234@gmail.com>
2023-03-14 01:13:28 -0700

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 
 const len = process.argv.length;
 
-if (len < 3){
-	console.log("No argument")
-}else if (len === 3){
-	console.log("Argument found")
-}else{
-	console.log("Arguments found")
+if (len < 3) {
+  console.log('No argument');
+} else if (len === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
