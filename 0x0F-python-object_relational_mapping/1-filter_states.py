bf1eeed78af9019d0c5f4c36c62623e6487a53a0
myrajarenga <myrajarenga1234@gmail.com>
2023-03-17 02:15:17 -0700

    script that filter  states from the database hbtn_0e_0_usa:

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
 
     try:
         cur = connection.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+        ORDER BY states.id")
         rows = cur.fetchall()
         for row in rows:
             print(row)
