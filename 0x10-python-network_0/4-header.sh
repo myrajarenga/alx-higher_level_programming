1a9cd44abe0332cd7b80e9043094fa1fee220dc9
myrajarenga <myrajarenga1234@gmail.com>
2023-04-01 01:15:46 -0700

    script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sS -H "X-School-User-Id: 98" "$1" | awk '/^\s*$/{flag=1;next} {if(flag)print}'
+curl -s -H "X-School-User-Id: 98" "$1"
