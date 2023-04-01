8c99bc59a859e5824b65eefcafdedf058ac6b86d
myrajarenga <myrajarenga1234@gmail.com>
2023-04-01 00:32:05 -0700

    script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+url="$1"response=$(curl -s -w '%{size_download}\n' -o /dev/null "$url")
