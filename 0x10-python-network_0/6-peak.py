06484e02336fd14847a956c48a9dd2da16c666f4
myrajarenga <myrajarenga1234@gmail.com>
2023-04-01 02:06:08 -0700

    function that finds apeak  in list of unsorted integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 
 def find_peak(list_of_integers):
-    """Find a peak in a list of unsorted integers"""
-    if not list_of_integers:
+    """
+    Find a peak in a list of unsorted integers using recursion.
+    """
+    length = len(list_of_integers)
+    if length == 0:
         return None
-    left = 0
-    right = len(list_of_integers) - 1
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-    return list_of_integers[left]
+    mid_index = length // 2
+    mid_value = list_of_integers[mid_index]
+    if length == 1:
+        return mid_value
+    elif length == 2:
+        return max(list_of_integers)
+    elif mid_value < list_of_integers[mid_index-1]:
+        return find_peak(list_of_integers[:mid_index])
+    elif mid_value < list_of_integers[mid_index+1]:
+        return find_peak(list_of_integers[mid_index+1:])
+    else:
+        return mid_value
