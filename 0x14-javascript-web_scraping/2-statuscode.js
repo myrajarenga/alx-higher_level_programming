6b6e38060ae372bd42c266e7559915c64ba26379
myrajarenga <myrajarenga1234@gmail.com>
2023-04-25 14:17:29 -0700

    script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+//script to priint status code of get request
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  console.log('code:', res.statusCode);
+})
